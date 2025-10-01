dafe3f5d3cfaa41b000e3273fc0527dc141049d1
ngusadeep <ngusadeep@gmail.com>
2025-10-01 05:38:33 +0300

    updated product service

--- src/modules/product/product.service.ts
+++ src/modules/product/product.service.ts
@@ -4,34 +4,55 @@ import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
 import { Repository } from 'typeorm';
+import { Category } from '../category/entities/category.entity';
+import { CategoryService } from '../category/category.service';
 
 @Injectable()
 export class ProductService {
   constructor(
     @InjectRepository(Product)
-    private readonly productRepo: Repository<Product>,
+    private readonly productRepository: Repository<Product>,
+    private readonly categoryService: CategoryService,
   ) {}
 
-  async create(createProductDto: CreateProductDto): Promise<Product> {
-    const newProduct = new Product();
+  async create(createProductDto: CreateProductDto) {
+    const category = await this.categoryService.findOne(
+      createProductDto.categoryId,
+    );
+    if (!category) {
+      return 'Category not found';
+    }
+    const newProduct = new Product(); // Creating a product object to initialize its values
     newProduct.name = createProductDto.name;
     newProduct.description = createProductDto.description;
     newProduct.price = createProductDto.price;
-    return await this.productRepo.save(newProduct);
+    newProduct.category = category; // Directly assign the found category
+    await this.productRepository.insert(newProduct); // insert the new product in the product table
+    return 'Product created successfully';
   }
 
   async findAll(): Promise<Product[]> {
-    return await this.productRepo.find();
+    return await this.productRepository.find();
   }
   async findOne(id: string): Promise<Product | null> {
-    return await this.productRepo.findOneBy({ product_id: id }); //returning a specific product
+    return await this.productRepository.findOneBy({ product_id: id }); //returning a specific product
   }
-  async update(updateProductDto: UpdateProductDto): Promise<string> {
-    await this.productRepo.update(updateProductDto.id, updateProductDto);
+  async update(updateProductDto: UpdateProductDto) {
+    const product = await this.findOne(updateProductDto.id);
+    if (!product) {
+      return 'Product not found';
+    }
+    await this.productRepository.update(updateProductDto.id, updateProductDto);
     return 'Product updated successfully!';
   }
+
   async remove(id: string): Promise<string> {
-    await this.productRepo.delete({ product_id: id }); //deleting the product
+    await this.productRepository.softDelete({ product_id: id }); // Mark as Deleted the product if exists, Does not check if entity exist in the database.
+    /*
+      the soft delete will not delete the object in table but it will make that object has been deleted by adding the date in deleted_on column. here we can retrive the value if needed.
+                                    OR
+      the delete will delete entire object from table and can't be retrived back
+      */
     return 'Product deleted successfully';
   }
 }
