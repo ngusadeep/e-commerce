948eca0980c53e7905b7a2319cf06edf67377908
ngusadeep <ngusadeep@gmail.com>
2025-10-01 05:49:20 +0300

    updated order modules

--- src/modules/order/order.module.ts
+++ src/modules/order/order.module.ts
@@ -3,10 +3,17 @@ import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Order } from './entities/order.entity';
-
+import { User } from '../user/entities/user.entity';
+import { Profile } from '../user/entities/profile.entity';
+import { Category } from '../category/entities/category.entity';
+import { Product } from '../product/entities/product.entity';
+import { ProductService } from '../product/product.service';
+import { CategoryService } from '../category/category.service';
 @Module({
-  imports: [TypeOrmModule.forFeature([Order])],
+  imports: [
+    TypeOrmModule.forFeature([Order, Product, User, Profile, Category]),
+  ],
   controllers: [OrderController],
-  providers: [OrderService],
+  providers: [OrderService, ProductService, CategoryService],
 })
 export class OrderModule {}
